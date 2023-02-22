1ff6581c19898f63f1d276195b4bfffbfc54e5e7
gaofeng222 <596642721@qq.com>
2023-02-22 13:20:14 +0800

    初始化项目

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,26 @@
+export default {
+  title: '高级前端工程师面试宝典',
+  lastUpdated: true,
+  description: 'Just playing around.',
+  themeConfig: {
+    sidebar: [
+      {
+        text: '指引',
+        items: [{ text: '写在前面', link: '/index' }]
+      },
+      {
+        text: '浏览器端',
+        items: [
+          {
+            text: 'dom',
+            items: [{ text: 'dom树是怎么生成的', link: '/browser/index' }]
+          }
+        ]
+      }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present Gaofeng'
+    }
+  }
+}
---
+++ docs/browser/index.md
@@ -0,0 +1,12 @@
+---
+title: Dom相关知识点
+editLink: true
+---
+
+### dom 生成的顺序
+
+[![pSjOHVH.png](https://s1.ax1x.com/2023/02/22/pSjOHVH.png)](https://imgse.com/i/pSjOHVH)
+
+浏览器渲染的顺序：
+
+1.构建 dom 树 =>2.构建 css 树 =>3.构建渲染树 =>4.节点布局 =>5.页面渲染 => 6.展示眼前
